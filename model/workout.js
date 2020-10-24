1469b75617d8a92f2e4cc3723f2fcf05c1cf8451
bmartinez2018 <biancamartinez295@icloud.com>
2020-10-24 12:08:07 -0500

    make it better

--- model/workout.js
+++ model/workout.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    //day, default date.now
     day: {
         type: Date,
         default: Date.now()
