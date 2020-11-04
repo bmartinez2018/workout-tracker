20c92e674899a855922bf7501548676a3a795f7b
bmartinez2018 <biancamartinez295@icloud.com>
2020-11-03 19:18:53 -0600

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", 
+    {
+        useNewUrlParser: true
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 
 require('./routes/api-routes')(app)
