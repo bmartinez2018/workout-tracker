ac22b89d3d7d86f367a8b755abfffe7708c74da7
bmartinez2018 <biancamartinez295@icloud.com>
2020-11-03 19:41:57 -0600

    editted gtingnore

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", 
     {
-        useNewUrlParser: true
+        useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false
